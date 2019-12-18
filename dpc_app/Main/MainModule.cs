92dd846c016f8e3860b4088ebc2f4cb3c0b86216
Jayward Tamares <jaywardtamares@outlook.com>
2019-12-18 22:14:38 +0800

    Add prism nuget

---
+++ dpc_app/Main/MainModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace Main
+{
+    public class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- dpc_app/dpc_app/dpc_app/App.xaml.cs
+++ dpc_app/dpc_app/dpc_app/App.xaml.cs
@@ -1,28 +1,49 @@
-﻿using System;
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace dpc_app
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+
+        }
+
+        //Handles the first page to be shown after the splash screen
+        protected override void OnInitialized()
         {
             InitializeComponent();
+            NavigationService.NavigateAsync("LandingPageView");
+        }
 
-            MainPage = new MainPage();
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
         }
 
         protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
+            // Handle when your app resumes
         }
     }
 }
