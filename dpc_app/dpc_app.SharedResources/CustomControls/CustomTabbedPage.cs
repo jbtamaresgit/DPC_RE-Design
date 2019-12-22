6be057109c9fd00c4fc129ab5013aed754dac73e
Jayward Tamares <jaywardtamares@outlook.com>
2019-12-22 14:58:25 +0800

    * Customize tabbar android  to make indicator color transparent

---
+++ dpc_app/dpc_app.SharedResources/CustomControls/CustomTabbedPage.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace dpc_app.SharedResources.CustomControls
+{
+    public class CustomTabbedPage : TabbedPage
+    {
+        public Color SelectedIconColor
+        {
+            get { return (Color)GetValue(SelectedIconColorProperty);  }
+            set { SetValue(SelectedIconColorProperty, value); }
+        }
+
+        public static readonly BindableProperty SelectedIconColorProperty = BindableProperty.Create(
+                nameof(SelectedIconColorProperty),
+                typeof(Color),
+                typeof(CustomTabbedPage),
+                Color.White);
+    }
+}
