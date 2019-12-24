88e494647e9eb9a3bd660667a99805ac656930cf
Jayward Tamares <jaywardtamares@outlook.com>
2019-12-24 18:52:30 +0800

    [iOS]
    * Update plist for montserrat font
    * Import montserrat font
    
    [Android]
    
    * Import montserrat font
    
    [Forms]
    
    * Update app.xaml for MontserratRegular font
    * Add header control template

--- dpc_app/Main/Views/MainTabbedPageView.xaml.cs
+++ dpc_app/Main/Views/MainTabbedPageView.xaml.cs
@@ -1,15 +1,16 @@
-﻿
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace Main.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainTabbedPageView : TabbedPage
+    public partial class MainTabbedPageView : Xamarin.Forms.TabbedPage
     {
         public MainTabbedPageView()
         {
             InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
