59c9dcc4505d0c3152299ed74c0aa7a3712df969
Jayward Tamares <jaywardtamares@outlook.com>
2019-12-28 13:22:53 +0800

    * Create custom bindable scroll view * Implement on going tournament design

--- dpc_app/dpc_app.SharedResources/CustomControls/CustomScrollView/HiddenScrollViewControl.cs
+++ dpc_app/dpc_app.SharedResources/CustomControls/CustomScrollView/HiddenScrollViewControl.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace dpc_app.SharedResources.CustomControls.CustomScrollView
 {
     public class HiddenScrollViewControl : ScrollView
     {
+        public static readonly BindableProperty IsScrollEnabledProperty = BindableProperty.Create(
+           nameof(IsScrollEnabled), typeof(bool), typeof(HiddenScrollViewControl), true);
+
+        public bool IsScrollEnabled
+        {
+            get { return (bool)GetValue(IsScrollEnabledProperty); }
+            set { SetValue(IsScrollEnabledProperty, value); }
+        }
+
+        public static readonly BindableProperty IsScrollVisibleProperty = BindableProperty.Create(
+           nameof(IsScrollVisible), typeof(bool), typeof(HiddenScrollViewControl), true);
+
+        public bool IsScrollVisible
+        {
+            get { return (bool)GetValue(IsScrollVisibleProperty); }
+            set { SetValue(IsScrollVisibleProperty, value); }
+        }
     }
 }
