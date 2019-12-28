3369147507554c0ec4ef0c49675b4b53ebbedcbf
Jayward Tamares <44351076+jbtamaresgit@users.noreply.github.com>
2019-12-28 13:35:11 +0800

    Feature/on going tournaments (#2)
    * * Add ff image nuget
    * Implement init for ff image
    
    * * Create custom bindable scroll view
    * Implement on going tournament design

---
+++ dpc_app/dpc_app/dpc_app.Android/CustomRenderers/CustomScrollView/HiddenScrollViewRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using dpc_app.Droid.CustomRenderers.CustomScrollView;
+using dpc_app.SharedResources.CustomControls.CustomScrollView;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(HiddenScrollViewControl), typeof(HiddenScrollViewRenderer))]
+namespace dpc_app.Droid.CustomRenderers.CustomScrollView
+{
+    public class HiddenScrollViewRenderer : ScrollViewRenderer
+    {
+        public HiddenScrollViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if(this.Element == null)
+            {
+                return;
+            }
+
+            if(e.OldElement != null)
+            {
+                e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+            }
+
+            if(e.NewElement != null)
+            {
+                e.NewElement.PropertyChanged += OnElementPropertyChanged;
+            }
+        }
+
+        private void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            HiddenScrollViewControl Control = Element as HiddenScrollViewControl;
+
+            if(ChildCount > 0)
+            {
+                GetChildAt(0).HorizontalScrollBarEnabled = Control.IsScrollEnabled;
+                GetChildAt(0).VerticalScrollBarEnabled = Control.IsScrollEnabled;
+            }
+        }
+    }
+}
