170a2503c8ac0d71ba25feca4cbdae56620356df
jbtamaresgit <jbtamaresgit@outlook.com>
2020-02-17 17:10:19 +0800

    * Implement design for tournaments tab * Implement view model for tournaments tab * Register tournaments view and view model in main module

---
+++ dpc_app/Main/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace Main.ViewModels
+{
+    public class BaseViewModel : BindableBase, INavigationAware
+    {
+
+        public BaseViewModel()
+        {
+
+        }
+
+        private bool _IsBusy = false;
+        public bool IsBusy
+        {
+            get { return _IsBusy; }
+            set { SetProperty(ref _IsBusy, value); }
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+            
+        }
+    }
+}
---
+++ dpc_app/Main/ViewModels/TournamentsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Main.Models;
+using MvvmHelpers;
+using Prism.Navigation;
+using System;
+
+namespace Main.ViewModels
+{
+    public class TournamentsViewModel : BaseViewModel, IInitialize
+    {
+        public TournamentsViewModel()
+        {
+
+        }
+
+        private ObservableRangeCollection<TournamentModel> _Tournaments;
+        public ObservableRangeCollection<TournamentModel> Tournaments
+        {
+            get { return _Tournaments; }
+            set { SetProperty(ref _Tournaments, value); }
+        }
+        
+        public void Initialize(INavigationParameters parameters)
+        {
+            double prize = 300000;
+            DateTime startDate = new DateTime(2019, 12, 05);
+            DateTime endDate = new DateTime(2020, 01, 14);
+
+            Tournaments = new ObservableRangeCollection<TournamentModel>()
+            {
+                new TournamentModel
+                {
+                    Title = "WePlay! Bukovel Minor 2020",
+                    Type = "MINOR",
+                    Prize = prize.ToString("C0"),
+                    Duration = $"{startDate.ToString("dd MMMM")} - {endDate.ToString("dd MMMM")}"
+                }
+            };
+        }
+    }
+}
