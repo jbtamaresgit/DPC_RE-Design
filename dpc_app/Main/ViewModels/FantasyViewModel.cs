eca77773e11795c952f2feb563adb685aa675687
jbtamaresgit <jbtamaresgit@outlook.com>
2020-02-19 14:00:42 +0800

    * Implement fantasy view model for its content * Implement design for fantasy view tab * Implement fantasy view model and view on main module
    [MainTabbedPageView]
    * Change bg color of the content

---
+++ dpc_app/Main/ViewModels/FantasyViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Main.Models;
+using MvvmHelpers;
+using Prism.Navigation;
+
+namespace Main.ViewModels
+{
+    public class FantasyViewModel : BaseViewModel, IInitialize
+    {
+        public FantasyViewModel()
+        {
+
+        }
+
+        private ObservableRangeCollection<FantasyTournamentModel> _FantasyTournaments;
+        public ObservableRangeCollection<FantasyTournamentModel> FantasyTournaments
+        {
+            get { return _FantasyTournaments; }
+            set { SetProperty(ref _FantasyTournaments, value); }
+        }
+
+        public void Initialize(INavigationParameters parameters)
+        {
+            FantasyTournaments = new ObservableRangeCollection<FantasyTournamentModel>()
+            {
+                new FantasyTournamentModel
+                {
+                    Status = "UPCOMING",
+                    Title = "DreamLeague Season 13",
+                    Type = "MAIN EVENT",
+                    RosterStatus = "ROSTER SET"
+                },
+                new FantasyTournamentModel
+                {
+                    Status = "UPCOMING",
+                    Title = "DreamLeague Season 13",
+                    Type = "MAIN EVENT",
+                    RosterStatus = "ROSTER SET"
+                }
+            };
+        }
+    }
+}
