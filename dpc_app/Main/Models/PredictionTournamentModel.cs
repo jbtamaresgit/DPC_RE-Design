46b5b4f9c6c4d5994e3dab0beecbe1f0d7b2c6f7
Jayward Tamares <44351076+jbtamaresgit@users.noreply.github.com>
2020-02-22 15:52:36 +0800

    * Implement predictions tab design and list content (#7)
    * Include predictions vm and view to main module
    
    AB#12

---
+++ dpc_app/Main/Models/PredictionTournamentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Main.Models
+{
+    public class PredictionTournamentModel
+    {
+        public string TournamentImageSrc { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public string Duration { get; set; }
+    }
+}
