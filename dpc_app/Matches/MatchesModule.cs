82621ecb3da114d0b555beb57084d8b4ea38dbe3
jbtamaresgit <jbtamaresgit@outlook.com>
2020-03-04 16:16:27 +0800

    * Implement tab item and tab text item bindable properties * Implement tab item design custom control content view

---
+++ dpc_app/Matches/MatchesModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace Matches
+{
+    public class MatchesModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ dpc_app/dpc_app.SharedResources/CustomControls/CustomTabbedPage/CustomTabs/TabTextItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace dpc_app.SharedResources.CustomControls.CustomTabbedPage.CustomTabs
+{
+    public abstract class TabTextItem : TabItem
+    {
+        public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create(
+            nameof(FontFamily),
+            typeof(string),
+            typeof(TabTextItem),
+            null,
+            BindingMode.OneWay);
+
+        public string FontFamily
+        {
+            get { return (string)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+
+        public static readonly BindableProperty LabelContentProperty = BindableProperty.Create(
+            nameof(LabelContent),
+            typeof(string),
+            typeof(TabTextItem),
+            string.Empty);
+
+        public string LabelContent
+        {
+            get { return (string)GetValue(LabelContentProperty); }
+            set { SetValue(LabelContentProperty, value); }
+        }
+
+        public static readonly BindableProperty LabelSizeProperty = BindableProperty.Create(
+            nameof(LabelSize),
+            typeof(double),
+            typeof(TabTextItem));
+
+        public double LabelSize
+        {
+            get { return (double)GetValue(LabelSizeProperty); }
+            set { SetValue(LabelSizeProperty, value); }
+        }
+
+        public static readonly BindableProperty UnselectedLabelColorProperty = BindableProperty.Create(
+            nameof(UnselectedLabelColor),
+            typeof(Color),
+            typeof(TabTextItem));
+
+        public Color UnselectedLabelColor
+        {
+            get { return (Color)GetValue(UnselectedLabelColorProperty); }
+            set { SetValue(UnselectedLabelColorProperty, value); }
+        }
+    }
+}
