b5a80509aa02a6653ba371adbe46a2526740f712
jbtamaresgit <jbtamaresgit@outlook.com>
2020-04-06 16:12:15 +0800

    * Create initial ui draft for dpc standings

--- dpc_app/DPCStandings/DPCStandingsModule.cs
+++ dpc_app/DPCStandings/DPCStandingsModule.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
 
 namespace DPCStandings
 {
-    public class DPCStandingsModule
+    public class DPCStandingsModule : IModule
     {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+           
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
---
+++ dpc_app/DPCStandings/Views/DPCStandingsOverview.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DPCStandings.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DPCStandingsOverview : ContentPage
+    {
+        public DPCStandingsOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
