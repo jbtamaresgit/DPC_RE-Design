0e04e2e8549163e873473e618bd7addc9bc8e564
jbtamaresgit <jbtamaresgit@outlook.com>
2020-04-06 15:20:07 +0800

    * Add team image source to model * Transform to collection view for the list in view

--- dpc_app/Tournaments/Models/TournamentUpdateContentModel.cs
+++ dpc_app/Tournaments/Models/TournamentUpdateContentModel.cs
@@ -3,6 +3,7 @@
     public class TournamentUpdateContentModel
     {
         public string TournamentImageSrc { get; set; }
+        public string TeamImageSrc { get; set; }
         public string TeamName { get; set; }
         public string TournamentUpdateContent { get; set; }
         public string DPCPoints { get; set; }
--- dpc_app/Tournaments/ViewModels/TournamentUpdatesViewModel.cs
+++ dpc_app/Tournaments/ViewModels/TournamentUpdatesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Navigation;
+﻿using MvvmHelpers;
+using Prism.Navigation;
 using Tournaments.Models;
 
 namespace Tournaments.ViewModels
@@ -13,11 +14,11 @@ namespace Tournaments.ViewModels
 
         public string PageTitle { get { return "Tournament Updates"; } }
 
-        private TournamentUpdateContentModel _Content;
-        public TournamentUpdateContentModel Content
+        private ObservableRangeCollection<TournamentUpdateContentModel> _TournamentUpdates;
+        public ObservableRangeCollection<TournamentUpdateContentModel> TournamentUpdates
         {
-            get { return _Content; }
-            set { SetProperty(ref _Content, value); }
+            get { return _TournamentUpdates; }
+            set { SetProperty(ref _TournamentUpdates, value); }
         }
 
         public override void OnNavigatedFrom(INavigationParameters parameters)
@@ -29,12 +30,17 @@ namespace Tournaments.ViewModels
         {
             base.OnNavigatedTo(parameters);
 
-            Content = new TournamentUpdateContentModel
+            TournamentUpdates = new ObservableRangeCollection<TournamentUpdateContentModel>
             {
-                TeamName = "Team Aster",
-                TournamentUpdateContent = " has won StarLadder ImbaTV Dota 2 Minor!",
-                DPCPoints = "140 DPC POINTS",
-                DPCPrize = "$72,000 USD"
+                new TournamentUpdateContentModel
+                {
+                    TournamentImageSrc = "https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2020/02/05134451/DOTA2_KYIV.jpg",
+                    TeamImageSrc = "https://liquipedia.net/commons/images/thumb/9/94/Team_Aster_logo.png/600px-Team_Aster_logo.png",
+                    TeamName = "Team Aster",
+                    TournamentUpdateContent = " has won StarLadder ImbaTV Dota 2 Minor!",
+                    DPCPoints = "140 DPC POINTS",
+                    DPCPrize = "$72,000 USD"
+                }
             };
         }
     }
