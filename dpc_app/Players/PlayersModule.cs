af5be21d11058aec41d4cde3620d87afd98a14a5
jbtamaresgit <jbtamaresgit@outlook.com>
2020-04-09 14:25:11 +0800

    * Implement rough idea of the design for the player list * Add player list module * Install req nugets

---
+++ dpc_app/Players/PlayersModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Players
+{
+    public class PlayersModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ dpc_app/Players/ViewModels/PlayerListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Navigation;
+
+namespace Players.ViewModels
+{
+    public class PlayerListViewModel : BaseViewModel
+    {
+        public PlayerListViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public string ImageSrc { get { return "https://i.axs.com/2019/11/67184-image_5dd4889332d17.jpg"; } }
+
+        public string PageTitle { get { return "Players"; } }
+
+
+        public override void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+    }
+}
