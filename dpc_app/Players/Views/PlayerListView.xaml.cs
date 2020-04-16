d57bd04474e502250e49d106ddb653ab4c3fe2e8
jbtamaresgit <jbtamaresgit@outlook.com>
2020-04-16 23:53:26 +0800

    Add close button to show when animation ends

--- dpc_app/Players/Views/PlayerListView.xaml.cs
+++ dpc_app/Players/Views/PlayerListView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,6 +13,8 @@ namespace Players.Views
         public PlayerListView()
         {
             InitializeComponent();
+
+            closeButton.IsVisible = false;
 
             InitTeamBadgeContainer();
         }
@@ -26,5 +30,19 @@ namespace Players.Views
             //    new Point(5, 50)
             //};
         }
+
+        private void GridSearchTappedGesture_Tapped(object sender, EventArgs e)
+        {
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            int currentMargin = 40;
+            double endPoint = (mainDisplayInfo.Width / mainDisplayInfo.Density) - currentMargin;
+
+            new Animation(callback: x => SearchBoxView.WidthRequest = x, start: SearchBoxView.Width,
+               end: endPoint).Commit(this, "SearchBoxAnimation", 16, 1000, Easing.SinIn, finished: (x, y) =>
+               {
+                   closeButton.IsVisible = true;
+               });
+        }
+
     }
 }
