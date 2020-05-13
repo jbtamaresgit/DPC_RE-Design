bb5afd86277fc22228424c6713e09145e65e4b91
jbtamaresgit <jbtamaresgit@outlook.com>
2020-05-13 22:30:15 +0800

    * Fix translateX value for slider

--- dpc_app/Predictions/Views/PopUp/WagerPopUpView.xaml.cs
+++ dpc_app/Predictions/Views/PopUp/WagerPopUpView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Rg.Plugins.Popup.Pages;
+using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Predictions.Views.PopUp
@@ -6,9 +8,36 @@ namespace Predictions.Views.PopUp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WagerPopUpView : PopupPage
     {
+        const int dividend = 10;
+        int SliderXValue = 0;
+
         public WagerPopUpView()
         {
             InitializeComponent();
+        }
+
+        private void SliderWager_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            double newStep = Math.Round(e.NewValue / dividend);
+            SliderWager.Value = newStep * dividend;
+            lblSliderValue.Text = SliderWager.Value.ToString();
+
+            if (SliderWager.Value >= 0 && SliderWager.Value <= 20)
+            {
+                SliderXValue = 45;        
+            }
+            else if(SliderWager.Value >= 30 && SliderWager.Value <= 40)
+            {
+                SliderXValue = 40;
+            }
+            else
+            {
+                SliderXValue = 37;
+            }
+
+
+            double TranslateXValue = SliderWager.Value * ((SliderWager.Width - SliderXValue) / SliderWager.Maximum);
+            lblSliderValue.TranslateTo(TranslateXValue, 0, 100);
         }
     }
 }
