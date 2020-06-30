b730ed10cd7e3eb8e640d5f5f682998b947c357e
jbtamaresgit <jbtamaresgit@outlook.com>
2020-06-30 21:50:28 +0800

    * Create fantasy module * Install required nugets * Include required dependencies * Include fantasy module into module catalog in shell

---
+++ dpc_app/Fantasy/FantasyModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace Fantasy
+{
+    public class FantasyModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
