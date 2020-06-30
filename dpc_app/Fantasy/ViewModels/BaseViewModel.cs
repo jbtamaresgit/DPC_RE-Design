05a8e221c987fca424dc20bbb41210338e1eff87
jbtamaresgit <jbtamaresgit@outlook.com>
2020-06-30 21:59:43 +0800

    * Add views and view models

---
+++ dpc_app/Fantasy/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+
+namespace Fantasy.ViewModels
+{
+    public class BaseViewModel : BindableBase, INavigatedAware
+    {
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
